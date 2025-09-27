9f48500cf5f4d9129c38a49e7bd67dafac872b06
Sergio Cortes <79613492+sergiocortes-dll@users.noreply.github.com>
2025-09-26 19:57:47 -0500

    added migrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,28 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using veterinaria.Models;
 
 namespace veterinaria.Data;
 
 public class AppDbContext : DbContext
 {
-    private readonly IConfiguration _configuration;
-
-    public AppDbContext(IConfiguration configuration)
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
-        _configuration = configuration;
     }
-    
+
     public DbSet<Vet> Vets { get; set; }
     public DbSet<Client> Clients { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseMySql(
-                _configuration.GetConnectionString("DefaultConnection"),
-                new MySqlServerVersion(new Version(8, 0, 36)));
-        }
-    }
 }
